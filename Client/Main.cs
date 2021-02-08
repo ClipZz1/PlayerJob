674522b0df9e73b11f4d62526a72fd810b206825
ClipZz1 <70108862+ClipZz1@users.noreply.github.com>
2021-02-08 12:35:19 -0800

    Add files via upload

---
+++ Client/Main.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using CitizenFX.Core;
+using static CitizenFX.Core.Native.API;
+
+namespace Client
+{
+    public class Main : BaseScript
+    {
+        public Main()
+        {
+            //Event handler for "OnClientResourceStart"
+            EventHandlers["onClientResourceStart"] += new Action<string>(OnClientResourceStart);
+        }
+
+        private void OnClientResourceStart(string resourceName)
+        {
+            if (GetCurrentResourceName() != resourceName) return;
+
+            //It will output this through the F8 key
+            Debug.WriteLine($"The resource {resourceName} has been started on the client.");
+        }
+    }
+}
